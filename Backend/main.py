85fa99081ed4f2b4d07e9d54cf6f4b1500d411e0
usefoundify <foundifyofficial@gmail.com>
2025-08-08 23:45:24 -0400

    Getting ready for prod build

--- Backend/main.py
+++ Backend/main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import api
@@ -51,9 +52,8 @@ def get_vehicle_location():
 
     return jsonify(d)
 
-
-
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    # app.run(host='0.0.0.0', port=5000, debug=True)
+    # For Prod
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
